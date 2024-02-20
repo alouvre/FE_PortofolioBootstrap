3b4ea838e8a2c8e7fc8cac4765cb37bd768fe98c
alflouvre <alifiamustika02@gmail.com>
2024-02-20 15:43:15 +0700

    Progress navbar 90% dengan Bootstrap 3 & JQUERY

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
-function(arguments) {
-    // body
-}
+/*global $*/
+
+// event pada saat link di klik
+$('.page-scroll').on('click', function(event) {
+
+    // ambil isi href
+    var tujuan = $(this).attr('href');
+    // tangkap elemen ybs
+    var elemenTujuan = $(tujuan);
+
+    // pindahkan scroll
+    $('html, body').animate({
+        scrollTop: elemenTujuan.offset().top - 50
+    }, 1000, 'swing');
+
+    // Mencegah default behavior dari tautan
+    event.preventDefault();
+
+});
